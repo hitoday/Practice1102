36e72811fa1c62c90cc2009556d57b6132c49a57
hitoday <bh460@naver.com>
2022-11-03 18:01:31 +0900

    1103 Controller 설계

--- src/main/java/com/example/Practice/CrudController.java
+++ src/main/java/com/example/Practice/CrudController.java
@@ -1,7 +1,30 @@
 package com.example.Practice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Controller
 public class CrudController {
+
+    private final CrudService crudService;
+
+    @Autowired
+    protected CrudController(CrudService crudService){
+        this.crudService = crudService;
+    }
+
+    @GetMapping("/Crud/Insert")
+    public void insertReview(){
+    }
+
+    @PostMapping("/Crud/Insert")
+    public String insertReview(CrudEntity crudEntity){
+        crudEntity.setCreatedDate(LocalDateTime.now());
+        return "Crud/List";
+    }
 }
